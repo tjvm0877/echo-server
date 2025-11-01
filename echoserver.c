17505d86735cea82acca16a8f407bb6478098311
tjvm0877 <itsmine096@gmail.com>
2025-11-02 02:55:27 +0900

    Switch to multi-thread echo server

--- echoserver.c
+++ echoserver.c
@@ -1,125 +1,38 @@
 #include "csapp.h"
 
-typedef struct
-{
-    int maxfd;                   /* Largest descriptor in read_set*/
-    fd_set read_set;             /* Set of all active descriptors */
-    fd_set ready_set;            /* Subset of descriptors ready for reading */
-    int nready;                  /* Number of ready descriptors from select */
-    int maxi;                    /* High water index into client array */
-    int clientfd[FD_SETSIZE];    /* Set of active descriptors */
-    rio_t clientrio[FD_SETSIZE]; /* Set of active read buffers */
-} pool;
-
-void init_pool(int listenfd, pool *p);
-void add_client(int connfd, pool *p);
-void check_clients(pool *p);
-
-int byte_cnd = 0; /* Counts total bytes received by server */
+void echo(int connfd);
+void *thread(void *vargp);
 
 int main(int argc, char **argv)
 {
-    int listenfd, connfd;
+    int listenfd, *connfdp;
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
-    static pool pool;
+    pthread_t tid;
+
     if (argc != 2)
     {
-        fprintf(stderr, "usage: %s <port>\n", argv[1]);
+        fprintf(stderr, "useage: %s <port>\n", argv[0]);
         exit(0);
     }
-    listenfd = Open_listenfd(argv[1]);
-    init_pool(listenfd, &pool);
+    listenfd = open_listenfd(argv[1]);
+
     while (1)
     {
-        /* Wait for listening/connected descriptor(s) to become ready */
-        pool.ready_set = pool.read_set;
-        pool.nready = Select(pool.maxfd + 1, &pool.ready_set, NULL, NULL, NULL);
-
-        /* If listening descriptor ready, add new client to pool */
-        if (FD_ISSET(listenfd, &pool.ready_set))
-        {
-            clientlen = sizeof(struct sockaddr_storage);
-            connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-            add_client(connfd, &pool);
-        }
-        /* Echo a text line from each ready connected descriptor */
-        check_clients(&pool);
+        clientlen = sizeof(struct sockaddr_storage);
+        connfdp = Malloc(sizeof(int));
+        *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Pthread_create(&tid, NULL, thread, connfdp);
     }
 }
 
-void init_pool(int listenfd, pool *p)
+/* Thread routine */
+void *thread(void *vargp)
 {
-    /* Initially, there are no connected descriptors */
-    p->maxi = -1;
-    for (int i = 0; i < FD_SETSIZE; ++i)
-        p->clientfd[i] = -1;
-
-    /* Initially, listenfd is only member of select read set */
-    p->maxfd = listenfd;
-    FD_ZERO(&p->read_set);
-    FD_SET(listenfd, &p->read_set);
-}
-
-void add_client(int connfd, pool *p)
-{
-    int i;
-    p->nready--;
-    for (i = 0; i < FD_SETSIZE; ++i) /* Find an available slot */
-        if (p->clientfd[i] < 0)
-        {
-            /* Add connected descriptor to the pool */
-            p->clientfd[i] = connfd;
-            Rio_readinitb(&p->clientrio[i], connfd);
-
-            /* Add the descriptor to descriptor set */
-            FD_SET(connfd, &p->read_set);
-
-            /* Update max descriptor and pool high water mark */
-            if (connfd > p->maxfd)
-                p->maxfd = connfd;
-            if (i > p->maxi)
-                p->maxi = i;
-            break;
-        }
-    if (i == FD_SETSIZE) /* Couln't find an empty slot */
-        app_error("add_client error: Too many clients");
-}
-
-void check_clients(pool *p)
-{
-    int i, connfd, n;
-    char buf[MAXLINE];
-    rio_t rio;
-
-    /* If the descriptor is ready, echo a text line from it */
-    for (i = 0; (i <= p->maxi) && (p->nready > 0); ++i)
-    {
-        connfd = p->clientfd[i];
-        rio = p->clientrio[i];
-
-        if ((connfd > 0) && (FD_ISSET(connfd, &p->ready_set)))
-        {
-            p->nready--;
-            if ((n = rio_readlineb(&rio, buf, MAXLINE)) != 0)
-            {
-                byte_cnd += n;
-                printf("Server received %d (%d total) bytes on fd %d\n", n, byte_cnd, connfd);
-                Rio_writen(connfd, buf, n);
-            }
-
-            /* EOF detetcted, remove descriptor from pool */
-            else
-            {
-                Close(connfd);
-                FD_CLR(connfd, &p->read_set);
-                p->clientfd[i] = -1;
-
-                /* shrink maxi if we removed the last active slot(s) */
-                if (i == p->maxi)
-                    while (p->maxi >= 0 && p->clientfd[p->maxi] < 0)
-                        p->maxi--;
-            }
-        }
-    }
+    int connfd = *((int *)vargp);
+    Pthread_detach(pthread_self());
+    Free(vargp);
+    echo(connfd);
+    Close(connfd);
+    return NULL;
 }
