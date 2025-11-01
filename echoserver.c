16ed65a2fb6f676efd192d8ede3d69ad7d306832
tjvm0877 <itsmine096@gmail.com>
2025-11-01 22:10:59 +0900

    Switch to multi-process echo server

--- echoserver.c
+++ echoserver.c
@@ -2,29 +2,56 @@
 
 void echo(int connfd);
 
+/* Reap all terminated children to prevent zombies (non-blocking) */
+void sigchld_handler(int sig)
+{
+    /* Reap any child that has finished without blocking the parent */
+    while (waitpid(-1, 0, WNOHANG) > 0)
+        ;
+    return;
+}
+
 int main(int argc, char **argv)
 {
     int listenfd, connfd;
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
-    char client_hostname[MAXLINE], client_port[MAXLINE];
 
+    /* Validate arguments: expect a single port number */
     if (argc != 2)
     {
         fprintf(stderr, "useage: %s <port>\n", argv[0]);
         exit(0);
     }
 
+    /* Install SIGCHLD handler to reap terminated children (avoid zombies) */
+    Signal(SIGCHLD, sigchld_handler);
+
+    /* Create a listening socket bound to the given port (string form) */
     listenfd = Open_listenfd(argv[1]);
+
     while (1)
     {
         clientlen = sizeof(struct sockaddr_storage);
+
+        /* Block waiting for the next incoming connection */
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
-        printf("Connected to (%s %s)\n", client_hostname, client_port);
-        echo(connfd);
-        printf("Closed to (%s %s)\n", client_hostname, client_port);
+
+        /* Fork a child to handle this client so the parent can keep accepting */
+        if (Fork() == 0)
+        {
+            /* Child process: no need for the listening socket */
+            Close(listenfd);
+
+            /* Serve this client connection (echo until EOF) */
+            echo(connfd);
+
+            /* Close the connected socket and exit child cleanly */
+            Close(connfd);
+            exit(0);
+        }
+
+        /* Parent process: close connected socket (child owns it now) */
         Close(connfd);
     }
-    exit(0);
 }
